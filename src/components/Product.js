09caf13b53b948452bf4ab5b7c86d7e38f4fd953
Alexander Frederik Vreeken <alexanderfrederikvreeken@MacBook-Air-13-Sander-Vreeken.local>
2020-08-29 10:58:21 +0200

    Added the description, information and photos to the product page.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,27 @@
 import React from 'react'
-import { AiFillHeart, AiOutlineClockCircle, AiOutlineEye, AiOutlineHeart } from 'react-icons/ai'
+import { AiFillHeart, AiOutlineClockCircle, AiOutlineEye, AiOutlineHeart, AiFillStar } from 'react-icons/ai'
 
 const Product = () => {
+
+    // Function to get the stars loaded within the page.
+    const getStarsElement = (number) => {
+        const stars = []
+        for (let n = 0; n < number; n++) {
+            stars.push(
+                <div className='product__star'>
+                    <AiFillStar />
+                </div>
+            )
+        }
+
+        return (
+            <div className='product__stars'>
+                {stars.map(star => {
+                    return star
+                })}
+            </div>    
+        )
+    }
 
     return (
         <div className='product'>
@@ -26,10 +46,19 @@ const Product = () => {
                 </div>
             </div>
             <div className='product__body'>
-                {/* <div className='product__photo'></div> */}
+                <div className='product__photos'>
+                    <div className='product__photo'></div>
+                    <div className='product__photo'></div>
+                    <div className='product__photo'></div>
+                </div>
                 <div className='product__information'>
-                    {/* <p className='product__rate'>€10,49</p> */}
-                    {/* <div className='btn btn-primary product__buy'>Buy this cake!</div> */}
+                    <p>€ 12,95</p>
+                    <p>Single pie</p>
+                    {getStarsElement(4)}
+                </div>
+                <div className='product__description'>
+                    <h6>About this item</h6>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
                 </div>
                 
             </div>
