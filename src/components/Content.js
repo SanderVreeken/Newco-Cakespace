eca4df45cf93888c691700a96934a68adec17ec1
Alexander Frederik Vreeken <alexanderfrederikvreeken@MacBook-Pro.local>
2020-08-31 20:03:41 +0200

    Mobile ready only without the menubar.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 import Product from './Product'
+import SearchBar from './SearchBar'
+import Owner from './Owner'
 
 const Content = () => {
 
     return (
         <div className='content'>
+            <div className='content__searchbar'>
+                <SearchBar />
+            </div>
             <Product />
+            <Owner />
         </div>
     )
 }
