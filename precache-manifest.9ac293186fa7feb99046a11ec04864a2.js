508bd894e11ffff1227abb0ee85fcd1a2c88b61b
Alexander Frederik Vreeken <alexanderfrederikvreeken@MacBook-Pro.local>
2020-08-31 20:17:30 +0200

    Updates

---
+++ precache-manifest.9ac293186fa7feb99046a11ec04864a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732c8bf42dda1d584bc9bf5481178749",
+    "url": "/Newco-Cakespace/index.html"
+  },
+  {
+    "revision": "dfd8dcdff7ed7e67564e",
+    "url": "/Newco-Cakespace/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6c372c35496fff3e0dcb",
+    "url": "/Newco-Cakespace/static/css/main.9ff48683.chunk.css"
+  },
+  {
+    "revision": "dfd8dcdff7ed7e67564e",
+    "url": "/Newco-Cakespace/static/js/2.23142231.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Newco-Cakespace/static/js/2.23142231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c372c35496fff3e0dcb",
+    "url": "/Newco-Cakespace/static/js/main.6b92eed6.chunk.js"
+  },
+  {
+    "revision": "2b42bec9a6a4d7903688",
+    "url": "/Newco-Cakespace/static/js/runtime-main.f3a82903.js"
+  }
+]);
