d5cc6f6eb9e41d0b1a93445e56f532a26cfdeb4e
Alexander Frederik Vreeken <alexanderfrederikvreeken@MacBook-Pro.local>
2020-09-07 14:26:36 +0200

    Changed to MacBook Air.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 
 import Result from './Result'
@@ -6,6 +6,7 @@ import Result from './Result'
 import { USERS } from '../Mockdata'
 
 const SearchBar = () => {
+    const [searchText, setSearchText] = useState('')
 
     return (
         <div className='searchbar'>
@@ -13,9 +14,11 @@ const SearchBar = () => {
                 <div className='searchbar__icon'>
                     <AiOutlineSearch />
                 </div>
-                <input className='searchbar__input' placeholder='Search Cakespace'></input>
+                <input className='searchbar__input' onChange={event => setSearchText(event.target.value)} placeholder='Search Cakespace' value={searchText}></input>
             </div>
-            <div className='searchbar__results'>
+            <div className='searchbar__results' style={{
+                visibility: searchText.length > 0 ? 'visible' : 'hidden'
+            }}>
                 {USERS.map(user => <Result user={user}/>)}
             </div>
         </div>
